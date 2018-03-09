fa0504cead611c9ea87f346182ce46d1514b2e32
Penghao <755407349@qq.com>
2018-03-09 18:19:42 +0800

    开启内存分页

--- loader.asm
+++ loader.asm
@@ -3,7 +3,6 @@
 ;	3.开启内存分页
 ;	4.重新放置内核
 ;	5.跳入内核
-[.bit16]
 start:
 	mov ax,cs
 	mov ds,ax	;ds=ax=0x1000
@@ -66,7 +65,6 @@ into_pro_mode:
 ;页目录表位置:0x20000~0x21000
 ;页表位置:0x21000~0x22000
 ;3.设置页目录表，开启内存分页模式
-[.bit32]
 into_page_mode:
 	mov ax,10h
 	mov gs,ax
@@ -74,24 +72,25 @@ into_page_mode:
 	mov al,'A'
 	mov [gs:((80 * 0 + 39) * 2)],ax ;屏幕第0行, 第39列。
 	;先清零要用到的内存区域
-	mov ax,0x0012
+	mov ax,0x0018
 	mov ds,ax
 	mov ebx,0x20000
 	mov ecx,0x800	;0x800*4=0x2000
 clear_mem:
-	mov dword [ds:bx],0
+	mov dword [ds:ebx],0
 	add bx,4
 	loop clear_mem
 	;清零完毕后只向页目录写第一个页目录项，因为之后的都用不到就不用设置
 	mov ebx,0x20000
-	mov dword [ds:bx],0x00021003	;指向第一个页表地址0x21000
+	mov dword [ds:ebx],0x00021003	;指向第一个页表地址0x21000
 	;写页表，1024个页表项，每个页表项4个字节
-	mov bx,0x21000
-	mov eax,0
+	mov ebx,0x21000
+	mov eax,0x3
 	mov ecx,0x400
 set_page_table:
-	mov dword [ds:bx],eax+0x3
+	mov dword [ds:ebx],eax
 	add eax,0x00001000
+	add ebx,4
 	loop set_page_table
 	;页目录表和页表都已创建，下面先将页目录表的地址送给cr3，再将cr0的最高位置1真正开启分页模式
 	mov eax,0x00020000
@@ -99,6 +98,13 @@ set_page_table:
 	mov eax,cr0
 	or eax,1000_0000_0000_0000_0000_0000_0000_0000b
 	mov cr0,eax
+	mov ax,0x0010
+	mov ds,ax
+	mov bx,0
+	mov ah,0x0c
+	mov al,'K'
+	mov word [ds:0],ax
+	jmp $
 
 ;4.重新放置内核
 reset_kernel:
@@ -142,9 +148,9 @@ mem_copy:
 
 gdt:
 	dd 0,0	;NULL!
-	dd 0x0000ffff,0x00009a01 ;code seg=0x10000,lim=0xffff sec=0x0008 loader代码段
-	dd 0x8000ffff,0x0000920b ;date seg=0xb800,lim=0xffff sec=0x0010 显存段
-	dd 0x0000ffff,0x00cf9200 ;date seg=0,lim=4GB sec=0x0012 全局数据段
+	dd 0x0000ffff,0x00009a01 ;code seg=0x10000,lim=0xffff sel=0x0008 loader代码段
+	dd 0x8000ffff,0x0000920b ;date seg=0xb800,lim=0xffff sel=0x0010 显存段
+	dd 0x0000ffff,0x00cf9200 ;date seg=0,lim=4GB sel=0x0018 全局数据段
 
 gdt_48: dw $-gdt-1	;gdt表描述符个数-1
 	dd gdt+0x10000	;32位gdt基地址
